39d3c99069c63782dc636dd79dac951fbea29fe3
Traorasp <miguelsolanopaz@outlook.com>
2022-06-27 19:58:22 -0400

    Made components files and basic route structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './components/Nav';
+import Shop from './components/Shop';
+import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div></div>
-  );
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+    );
 }
 
 export default App;
