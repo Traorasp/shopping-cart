611600b0ccd006f3f68a4431b71c1b676276a9d4
Traorasp <miguelsolanopaz@outlook.com>
2022-06-28 12:05:21 -0400

    Added home introduction

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Welcome to Fntasyne</h1>
+      <h2>About</h2>
+      <p>
+        Welcome to Fantasyne the only online store to sell and deliver
+        fantasy matreials right to your door. We have a multitude of 
+        unique and magical materials in stock from mithril to dragon scales.
+        Whatever you need we have.
+      </p>
+      <Link to="/shop">Visit Shop!</Link> |{" "}
+      <h2>Contact</h2>
+      <p>Oznik the Dwarve in Oarth</p>
+      <p>Email: fantasyne@AnotherWorld.com</p>
+      <p>Phone: 123-456-7891</p>
+    </div>
   );
 }
 
--- src/components/Nav.test.js
+++ src/components/Nav.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { screen, render } from "@testing-library/react";
 import Nav from "./Nav";
 import {BrowserRouter as Router} from 'react-router-dom';
-
+q
 describe("Nav component", () => {
     it("Renders correct heading", () => {
         const {getByRole} = render(<Router><Nav /></Router>);
